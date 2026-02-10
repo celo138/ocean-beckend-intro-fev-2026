0201d0de8ce0f3317553bbef2ad7aa6a60b25124
Marcelo Medeiros <85379101+celo138@users.noreply.github.com>
2026-02-10 00:38:31 +0000

    fix: correct spelling and improve character retrieval logic; add HTTP request file for creating a character

--- index.js
+++ index.js
@@ -5,20 +5,38 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
 app.get('/oi', (req, res) => {
-  res.send('Ola mundo') 
-})  
-const lista = [ 'Rick Sanches', 'Morty Smith', 'Summer Smith']
+  res.send('Olá, mundo!')
+})
+
+const lista = ['Rick Sanchez', 'Morty Smith', 'Beth Smith']
+
 app.get('/personagens', (req, res) => {
   res.send(lista)
 })
+
 app.get('/personagens/:id', (req, res) => {
   const id = req.params.id
-  const personagens = lista[id]
-  res.send(lista[id-1])
-  res.send(personagens) 
-  
+
+  const personagem = lista[id - 1]
+
+  res.send(personagem)
 })
+
+// Informo ao Express que o Body da Requisição está em JSON
+app.use(express.json())
+
+app.post('/personagens', (req, res) => {
+  const novoPersonagem = req.body.nome
+
+  lista.push(novoPersonagem)
+
+  res.send("Novo personagem adicionado com sucesso!")
+})
+
+
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
